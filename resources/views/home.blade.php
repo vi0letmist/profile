9a5a01ffb10e6cf0e24ac410eec3a33098e3c099
unknown <jaffar.jatmiko.jati@mail.ugm.ac.id>
2018-09-26 08:21:09 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/profile.css">
+    <title>Welcome, to my profile!</title>
+  </head>
+  <body>
+    <!-- <div class="container"> -->
+    	<div class="kiri">
+    		    <!--<figcaption>Jaffar Jatmiko Jati</figcaption>-->
+                <img src="svg/IMG_20180722_1111100333.jpg">
+            
+        </div>
+    	<div class="kanan">
+    		<h1>Welcome To My Profile</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <a href="/bio" class="cta">Profile</a>
+    	</div>
+
+    <!-- </div> -->
+  </body>
+</html>
